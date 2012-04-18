bb8c96d8967879ba86fd59857a1696b23d664de5
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-04-18 18:34:13 +0200

    userspace/include/hw/fpga_regs.h: changed PPSGen base address for the HAL

--- userspace/include/hw/fpga_regs.h
+++ userspace/include/hw/fpga_regs.h
@@ -13,7 +13,7 @@ extern volatile uint8_t *_fpga_base_virt;
 #define _fpga_writel(reg, val){ *(volatile uint32_t *)(FPGA_BASE_ADDR + (reg)) = (val); }
 #define _fpga_readl(reg) (*(volatile uint32_t *)(FPGA_BASE_ADDR + (reg)))
 
-#define FPGA_BASE_PPS_GEN 	   0x52000
+#define FPGA_BASE_PPS_GEN 	   0x10500
 #define FPGA_BASE_RTU 	   0x60000
 
 #endif
