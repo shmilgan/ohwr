7c75c8b10d6a3de310851cc2fd4cff0bfcc2343c
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-04-27 22:26:41 +0200

    userspace/libswitchhw: added box fan control pin (on by default)

--- userspace/libswitchhw/pio_pins.c
+++ userspace/libswitchhw/pio_pins.c
@@ -13,10 +13,12 @@
 //const pio_pin_t PIN_main_fpga_nrst[] = {{ PIOA, 5, PIO_MODE_GPIO, PIO_OUT }, {0}};
 
 const pio_pin_t PIN_mbl_reset_n[] =  {{PIOE, 1, PIO_MODE_GPIO, PIO_OUT_1}, {0}};
+const pio_pin_t PIN_mbl_box_fan_en[] =  {{PIOB, 20, PIO_MODE_GPIO, PIO_OUT_1}, {0}};
 
 const pio_pin_t * _all_cpu_gpio_pins[] =
 {
 	PIN_mbl_reset_n,
+	PIN_mbl_box_fan_en,
 	0
 };
 
