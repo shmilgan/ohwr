561509513f0019dd056016b9001582a961583822
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-06-12 12:18:32 +0200

    userspace/rootfs_override: fixes in startup scripts/config files

--- userspace/rootfs_override/wr/sbin/startup-mb.sh
+++ userspace/rootfs_override/wr/sbin/startup-mb.sh
@@ -3,8 +3,8 @@
 echo "Starting up WR Switch (18-ports MiniBackplane version)..."
 export WR_HOME="/wr"
 
-$WR_HOME/bin/load-fpga $WR_HOME/lib/firmware/8ports_mb.bin
-$WR_HOME/bin/lm32-loader $WR_HOME/lib/firmware/rt_cpu.bin
+$WR_HOME/bin/load-virtex $WR_HOME/lib/firmware/8ports_mb.bin
+$WR_HOME/bin/load-lm32 $WR_HOME/lib/firmware/rt_cpu.bin
 insmod $WR_HOME/lib/modules/wr_vic.ko
 insmod $WR_HOME/lib/modules/wr-nic.ko
 insmod $WR_HOME/lib/modules/wr_rtu.ko
