ffd8d8deade8d8d0f4dbc2df53c55f04997b8d06
Benoit Rat <benoit@sevensols.com>
2012-06-21 16:38:24 +0200

    usb-loader:Adding write & run support for DDR (live loading)

--- usb-loader/samba_applets/isp-project/extram/main.c
+++ usb-loader/samba_applets/isp-project/extram/main.c
@@ -83,44 +83,69 @@
 //------------------------------------------------------------------------------
 struct _Mailbox {
 
-    /// Command send to the monitor to be executed.
-    unsigned int command;
-    /// Returned status, updated at the end of the monitor execution.
-    unsigned int status;
+	/// Command send to the monitor to be executed.
+	unsigned int command;
+	/// Returned status, updated at the end of the monitor execution.
+	unsigned int status;
 
-    /// Input Arguments in the argument area
-    union {
+	/// Input Arguments in the argument area
+	union {
 
-        /// Input arguments for the Init command.
-        struct {
+		/// Input arguments for the Init command.
+		struct {
 
-            /// Communication link used.
-            unsigned int comType;
-            /// Trace level.
-            unsigned int traceLevel;
-            /// External memory voltage selection.
-            unsigned int VddMemSel;
-            /// External RAM type.
-            unsigned int ramType;
-            /// External RAM bus width.
-            unsigned int dataBusWidth;
-            /// External DDRAM Model.
-            unsigned int ddrModel;
+			/// Communication link used.
+			unsigned int comType;
+			/// Trace level.
+			unsigned int traceLevel;
+			/// External memory voltage selection.
+			unsigned int VddMemSel;
+			/// External RAM type.
+			unsigned int ramType;
+			/// External RAM bus width.
+			unsigned int dataBusWidth;
+			/// External DDRAM Model.
+			unsigned int ddrModel;
 
-        } inputInit;
+		} inputInit;
 
-        /// Output arguments for the Init command.
-        struct {
+		/// Output arguments for the Init command.
+		struct {
 
-            /// Memory size.
-            unsigned int memorySize;
-            /// Buffer address.
-            unsigned int bufferAddress;
-            /// Buffer size.
-            unsigned int bufferSize;
-        } outputInit;
-    } argument;
+			/// Memory size.
+			unsigned int memorySize;
+			/// Buffer address.
+			unsigned int bufferAddress;
+			/// Buffer size.
+			unsigned int bufferSize;
+		} outputInit;
+
+		/// Input arguments for the Write command.
+		struct {
+
+			/// Buffer address.
+			unsigned int bufferAddr;
+			/// Buffer size.
+			unsigned int bufferSize;
+			/// Memory offset.
+			unsigned int memoryOffset;
+
+		} inputWrite;
+
+		/// Output arguments for the Write command.
+		struct {
+
+			/// Bytes written.
+			unsigned int bytesWritten;
+
+		} outputWrite;
+
+	} argument;
 };
+
+//Use to jump to memory space
+typedef void VFptr(void);
+
 
 
 //------------------------------------------------------------------------------
@@ -144,22 +169,22 @@ int r_seed;
 
 void my_srand(int seed)
 {
-    r_seed = seed;
+	r_seed = seed;
 }
 
 int my_rand()
 {
-    r_seed *= 101028131;
-    r_seed += 12345;
-    return r_seed;
+	r_seed *= 101028131;
+	r_seed += 12345;
+	return r_seed;
 }
 
 extern int mem_test(unsigned long _start, unsigned long _end, unsigned long pattern_unused);
 
 static unsigned char ExtRAM_TestOk(void)
 {
-    unsigned int i;
-    unsigned int *ptr = (unsigned int *) EXTRAM_ADDR;
+	unsigned int i;
+	unsigned int *ptr = (unsigned int *) EXTRAM_ADDR;
 	TRACE_INFO( "External mem addr: %x\n\r", EXTRAM_ADDR);
 
 	return	mem_test((EXTRAM_ADDR), (EXTRAM_ADDR) + 0x4000000 - 0x4, 0) ? 0: 1;
@@ -168,128 +193,175 @@ static unsigned char ExtRAM_TestOk(void)
 
 void delay(int x)
 {
-    while(x--) asm volatile("nop");
+	while(x--) asm volatile("nop");
 }
 //------------------------------------------------------------------------------
 /// Applet code for initializing the external RAM.
 //------------------------------------------------------------------------------
 int main(int argc, char **argv)
 {
-    struct _Mailbox *pMailbox = (struct _Mailbox *) argv;
-    unsigned int ramType = 0;
-    unsigned int dataBusWidth = 0;
-    unsigned int ddrModel = 0;
+	struct _Mailbox *pMailbox = (struct _Mailbox *) argv;
+	unsigned int ramType = 0;
+	unsigned int bufferSize, bufferAddr, memoryOffset;
+	unsigned int bytesToWrite;
+	unsigned int tempBufferAddr;
+	unsigned int dataBusWidth = 0;
+	unsigned int ddrModel = 0;
 
-    LowLevelInit();
+	LowLevelInit();
 
-    TRACE_CONFIGURE_ISP(DBGU_STANDARD, 115200, BOARD_MCK);
-    
-    
-
-/*    *AT91C_PMC_PCER = AT91C_ID_PIOA;
-    *AT91C_PIOA_PDR = (1<<31);
-    *AT91C_PIOA_OER = (1<<31);
-    *AT91C_PIOA_BSR = (1<<31);
-    *AT91C_PIOA_PER = (1<<0);
-    *AT91C_PIOA_OER = (1<<0);
-    
-    *AT91C_PMC_PCKR = (1<<8); // select master clock
-    *AT91C_PMC_SCER = (1<<8); // ENABLE PCK0*/
-    
-    TRACE_INFO("Statup: PMC_MCKR %x MCK = %d command = %d\n\r", *AT91C_PMC_MCKR, BOARD_MCK, pMailbox->command);
-    // ----------------------------------------------------------
-    // INIT:
-    // ----------------------------------------------------------
-    if (pMailbox->command == APPLET_CMD_INIT) {
-
-        // Initialize PMC
-//	BOARD_RemapRam();
+	TRACE_CONFIGURE_ISP(DBGU_STANDARD, 115200, BOARD_MCK);
 
 
-        // Enable User Reset
-        AT91C_BASE_RSTC->RSTC_RMR |= AT91C_RSTC_URSTEN | (0xA5<<24);
-        
 
-        ramType = pMailbox->argument.inputInit.ramType;
-        dataBusWidth = pMailbox->argument.inputInit.dataBusWidth;
-        ddrModel = pMailbox->argument.inputInit.ddrModel;
+	/*    *AT91C_PMC_PCER = AT91C_ID_PIOA;
+	 *AT91C_PIOA_PDR = (1<<31);
+	 *AT91C_PIOA_OER = (1<<31);
+	 *AT91C_PIOA_BSR = (1<<31);
+	 *AT91C_PIOA_PER = (1<<0);
+	 *AT91C_PIOA_OER = (1<<0);
 
-//#if (DYN_TRACES == 1)
-//        traceLevel = pMailbox->argument.inputInit.traceLevel;
-//#endif
+	 *AT91C_PMC_PCKR = (1<<8); // select master clock
+	 *AT91C_PMC_SCER = (1<<8); // ENABLE PCK0*/
 
-        TRACE_INFO("-- EXTRAM ISP Applet %s --\n\r", SAM_BA_APPLETS_VERSION);
-        TRACE_INFO("-- %s\n\r", BOARD_NAME);
-        TRACE_INFO("-- Compiled: %s %s %s --\n\r", __DATE__, __TIME__, __GIT__);
-        TRACE_INFO("INIT command:\n\r");
+	TRACE_INFO("Statup: PMC_MCKR %x MCK = %d command = %d\n\r", *AT91C_PMC_MCKR, BOARD_MCK, pMailbox->command);
+	// ----------------------------------------------------------
+	// INIT:
+	// ----------------------------------------------------------
+	if (pMailbox->command == APPLET_CMD_INIT) {
 
-        TRACE_INFO("\tCommunication link type : %d\n\r", pMailbox->argument.inputInit.comType);
-        TRACE_INFO("\tData bus width : %d bits\n\r", dataBusWidth);
-        if (ramType == TYPE_SDRAM) {
-           TRACE_INFO("\tExternal RAM type : %s\n\r", "SDRAM");
-        }
-        else {
-            if (ramType == TYPE_DDRAM) {
-                TRACE_INFO("\tExternal RAM type : %s\n\r", "DDRAM");
-            }
-            else {
-                TRACE_INFO("\tExternal RAM type : %s\n\r", "PSRAM");
-            }
-        }
+		// Initialize PMC
+		//	BOARD_RemapRam();
+
+
+		// Enable User Reset
+		AT91C_BASE_RSTC->RSTC_RMR |= AT91C_RSTC_URSTEN | (0xA5<<24);
+
+
+		ramType = pMailbox->argument.inputInit.ramType;
+		dataBusWidth = pMailbox->argument.inputInit.dataBusWidth;
+		ddrModel = pMailbox->argument.inputInit.ddrModel;
+
+		//#if (DYN_TRACES == 1)
+		//        traceLevel = pMailbox->argument.inputInit.traceLevel;
+		//#endif
+
+		TRACE_INFO("-- EXTRAM ISP Applet %s --\n\r", SAM_BA_APPLETS_VERSION);
+		TRACE_INFO("-- %s\n\r", BOARD_NAME);
+		TRACE_INFO("-- Compiled: %s %s %s --\n\r", __DATE__, __TIME__, __GIT__);
+		TRACE_INFO("INIT command:\n\r");
+
+		TRACE_INFO("\tCommunication link type : %d\n\r", pMailbox->argument.inputInit.comType);
+		TRACE_INFO("\tData bus width : %d bits\n\r", dataBusWidth);
+		if (ramType == TYPE_SDRAM) {
+			TRACE_INFO("\tExternal RAM type : %s\n\r", "SDRAM");
+		}
+		else {
+			if (ramType == TYPE_DDRAM) {
+				TRACE_INFO("\tExternal RAM type : %s\n\r", "DDRAM");
+			}
+			else {
+				TRACE_INFO("\tExternal RAM type : %s\n\r", "PSRAM");
+			}
+		}
 
 #if defined(at91cap9) || defined(at91sam9m10) || defined(at91sam9g45)
-        TRACE_INFO("\tInit EBI Vdd : %s\n\r", (pMailbox->argument.inputInit.VddMemSel)?"3.3V":"1.8V");   
-        BOARD_ConfigureVddMemSel(pMailbox->argument.inputInit.VddMemSel);
+		TRACE_INFO("\tInit EBI Vdd : %s\n\r", (pMailbox->argument.inputInit.VddMemSel)?"3.3V":"1.8V");
+		BOARD_ConfigureVddMemSel(pMailbox->argument.inputInit.VddMemSel);
 #endif //defined(at91cap9)
 
-        if (pMailbox->argument.inputInit.ramType == TYPE_SDRAM) {
-            // Configure SDRAM controller
-            TRACE_INFO("\tInit SDRAM...\n\r");
+		if (pMailbox->argument.inputInit.ramType == TYPE_SDRAM) {
+			// Configure SDRAM controller
+			TRACE_INFO("\tInit SDRAM...\n\r");
 #if defined(PINS_SDRAM)
-            BOARD_ConfigureSdram(dataBusWidth);
+			BOARD_ConfigureSdram(dataBusWidth);
 #endif
-        }
-        else if (pMailbox->argument.inputInit.ramType == TYPE_PSRAM) {
-            TRACE_INFO("\tInit PSRAM...\n\r");   
+		}
+		else if (pMailbox->argument.inputInit.ramType == TYPE_PSRAM) {
+			TRACE_INFO("\tInit PSRAM...\n\r");
 #if defined(at91sam3u4)            
-            BOARD_ConfigurePsram();
+			BOARD_ConfigurePsram();
 #endif            
-        }
-        else { 
-            // Configure DDRAM controller
+		}
+		else {
+			// Configure DDRAM controller
 #if defined(at91cap9dk) || defined(at91sam9m10) || defined(at91sam9g45)
-            TRACE_INFO("\tInit DDRAM ... (model : %d)\n\r", ddrModel);
-	    BOARD_ConfigureVddMemSel(VDDMEMSEL_1V8);
-            BOARD_ConfigureDdram(0, dataBusWidth);
-//	    ddramc_hw_init();
+			TRACE_INFO("\tInit DDRAM ... (model : %d)\n\r", ddrModel);
+			BOARD_ConfigureVddMemSel(VDDMEMSEL_1V8);
+			BOARD_ConfigureDdram(0, dataBusWidth);
+			//	    ddramc_hw_init();
 #endif
-        }
-       TRACE_INFO("\tInit successful.\n\r");
-    }
-    
-    if (pMailbox->command == APPLET_CMD_WRITE || pMailbox->command == APPLET_CMD_READ) 
-    {
-	    
-	    // Test external RAM access
-	    if (ExtRAM_TestOk()) {
-		    pMailbox->status = APPLET_SUCCESS;
-	    }
-	    else {
-		    pMailbox->status = APPLET_FAIL;
-	    }
-	    
-	    pMailbox->argument.outputInit.bufferAddress = ((unsigned int) &end);
-	    pMailbox->argument.outputInit.bufferSize = 0;
-	    pMailbox->argument.outputInit.memorySize = EXTRAM_SIZE;
-    }
-    
+		}
+		TRACE_INFO("\tInit successful.\n\r");
+	}
 
-    // Acknowledge the end of command
-    TRACE_INFO("\tEnd of applet (command : %x --- status : %x)\n\r", pMailbox->command, pMailbox->status);
+	// ----------------------------------------------------------
+	// LIST_BAD_BLOCK: (Check DDR)
+	// ----------------------------------------------------------
+	else if (pMailbox->command == APPLET_CMD_LIST_BAD_BLOCKS)
+	{
 
-    // Notify the host application of the end of the command processing
-    pMailbox->command = ~(pMailbox->command);
+		// Test external RAM access
+		if (ExtRAM_TestOk()) {
+			pMailbox->status = APPLET_SUCCESS;
+		}
+		else {
+			pMailbox->status = APPLET_FAIL;
+		}
 
-    return 0;
+		pMailbox->argument.outputInit.bufferAddress = ((unsigned int) &end);
+		pMailbox->argument.outputInit.bufferSize = 0;
+		pMailbox->argument.outputInit.memorySize = EXTRAM_SIZE;
+	}
+
+
+	// ----------------------------------------------------------
+	// WRITE:
+	// ----------------------------------------------------------
+	else if (pMailbox->command == APPLET_CMD_WRITE)
+	{
+		memoryOffset = pMailbox->argument.inputWrite.memoryOffset;
+		bufferAddr = pMailbox->argument.inputWrite.bufferAddr;
+		bytesToWrite = pMailbox->argument.inputWrite.bufferSize;
+		tempBufferAddr = bufferAddr+memoryOffset;
+
+		TRACE_INFO("WRITE arguments : offset 0x%x, run 0x%x, of 0x%x Bytes\n\r",
+				memoryOffset, tempBufferAddr, bytesToWrite);
+
+		pMailbox->argument.outputWrite.bytesWritten = 0;
+
+		/*
+		 * We must define the following
+		 * 	-  MACH_TYPE_xxx
+		 * 	- Setup the kernel tagged list (http://www.arm.linux.org.uk/developer/booting.php#4)
+		 * 		 first 16KiB of RAM.
+		 *
+		 * 		we recommend to load at 32KiB into RAM
+		 */
+
+		//Fake the end of the applet because we will not be able to do anything after this step
+		if (bytesToWrite < EXTRAM_SIZE)
+		{
+				pMailbox->status = APPLET_SUCCESS;
+		}
+		else
+		{
+			pMailbox->status = APPLET_FAIL;
+		}
+		pMailbox->command = ~(pMailbox->command);
+
+		//Going to
+		((VFptr *)(EXTRAM_ADDR+memoryOffset))();
+
+	}
+
+exit :
+	// Acknowledge the end of command
+	TRACE_INFO("\tEnd of applet (command : %x --- status : %x)\n\r", pMailbox->command, pMailbox->status);
+
+	// Notify the host application of the end of the command processing
+	pMailbox->command = ~(pMailbox->command);
+
+	return 0;
 }
 
