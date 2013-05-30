c81c377b64db5574148e05e8a8b6220cea3968dd
Maciej Lipinski <maciej.lipinski@cern.ch>
2013-05-30 20:45:43 +0200

    [sbin] update of startup script

--- userspace/rootfs_override/wr/sbin/startup-mb.sh
+++ userspace/rootfs_override/wr/sbin/startup-mb.sh
@@ -13,10 +13,28 @@ done
 tfpga=$($WR_HOME/bin/shw_ver -F)
 
 $WR_HOME/bin/load-virtex $WR_HOME/lib/firmware/18p_mb-${tfpga}.bin
+
+###### v4-dev stuff ############################################################
+#
+# you might need to replace FPGA bitstream with your custom
+# 
+# $WR_HOME/bin/load-virtex $WR_HOME/lib/firmware/18p_mb-LX240T-your-name.bin
+################################################################################
+
 $WR_HOME/bin/load-lm32 $WR_HOME/lib/firmware/rt_cpu.bin
 insmod $WR_HOME/lib/modules/at91_softpwm.ko
 insmod $WR_HOME/lib/modules/wr_vic.ko
 insmod $WR_HOME/lib/modules/wr-nic.ko macaddr=$val
 insmod $WR_HOME/lib/modules/wr_rtu.ko
+
+###### v4-dev stuff ############################################################
+#
+# uncomment below if you want to use pstats
+# 
+# insmod $WR_HOME/lib/modules/wr_pstats.ko
+# 
+################################################################################
+
+
 $WR_HOME/sbin/start-daemons.sh
 
