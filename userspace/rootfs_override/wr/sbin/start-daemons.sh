ff867c60d3152c14f6d6f06c09fd97948d26f158
Alessandro Rubini <rubini@gnudd.com>
2013-11-25 12:26:25 +0100

    general: add PPSi as an alternative to ptp-noposix
    The PPSi daemon can now be selected in Kconfig. This commit
    changes configuration and the build scripts to support either of them.
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- userspace/rootfs_override/wr/sbin/start-daemons.sh
+++ userspace/rootfs_override/wr/sbin/start-daemons.sh
@@ -6,5 +6,17 @@ export WR_HOME="/wr"
 
 $WR_HOME/bin/wrsw_hal -c $WR_HOME/etc/wrsw_hal.conf &> /dev/kmsg &
 $WR_HOME/bin/wrsw_rtud >& /dev/kmsg &
-$WR_HOME/bin/ptpd -A -c >& /dev/null &
+
+# run ptp-noposix or ppsi, whatever is installed
+if [ -x $WR_HOME/bin/ptpd ]; then
+    $WR_HOME/bin/ptpd -A -c >& /dev/null &
+    exit 0
+fi
+if [ -x $WR_HOME/bin/ppsi ]; then
+    $WR_HOME/bin/pppsi >& /dev/kmsg &
+    exit 0
+fi
+echo "No WR-PTP dameon found" >&2
+exit 1
+
 
