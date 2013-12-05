1f97c54f24ac1bbc4f4308a3010db85b959587b5
José Luis Gutiérrez <jlgutierrez@ugr.es>
2013-12-05 19:04:03 +0100

    /var/www/management.php adding halt-reboot-rw-ro features options
    Adding new features to the management parts:
    	- Halting system
    	- Rebooting system
    	- Remounting filesystem as rw
    	- Remounting filesystem as ro

--- userspace/rootfs_override/var/www/management.php
+++ userspace/rootfs_override/var/www/management.php
@@ -14,13 +14,24 @@
 			<br><br><br><br><br><br>
 			<center>
 				<form action="exe_program.php" method="post">
-				<input type="hidden" name="cmd" value="halt">
+				Halt system: <input type="hidden" name="cmd" value="halt">
 				<input type="submit" value="Halt switch">
 				</form>
 				<form action="exe_program.php" method="post">
-				<input type="hidden" name="cmd" value="reboot">
+				Reboot system: <input type="hidden" name="cmd" value="reboot">
 				<input type="submit" value="Reboot switch">
 				</form>
+				</form>
+				<form action="exe_program.php" method="post">
+				Mount partition as rw: <input type="hidden" name="cmd" value="rw">
+				<input type="submit" value="Remount">
+				</form>
+				</form>
+				<form action="exe_program.php" method="post">
+				Mount partition as ro: <input type="hidden" name="cmd" value="ro">
+				<input type="submit" value="Remount">
+				</form>
+				
 			</center>
        
         </div>
