d54a1c6495ea97ca2c138e7190f7059652ed8426
Alessandro Rubini <rubini@gnudd.com>
2013-12-15 17:34:04 +0100

    softpll-unify: header changes (no effect on rt_cpu.bin)
    use irq.h from wrpc-sw; it only differs in white space.
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- rt/include/irq.h
+++ rt/include/irq.h
@@ -3,8 +3,11 @@
 
 static inline void clear_irq()
 {
-  unsigned int val = 1;
-  asm volatile ("wcsr ip, %0"::"r"(val));
+	unsigned int val = 1;
+	asm volatile ("wcsr ip, %0"::"r" (val));
 }
+
+void disable_irq();
+void enable_irq();
 
 #endif
