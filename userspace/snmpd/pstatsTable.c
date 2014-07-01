2caf601574667a299fa34afb6a989040b127da93
Alessandro Rubini <rubini@gnudd.com>
2014-07-01 17:23:29 +0200

    snmp: final step, it works with two remaining buglets
    This removes all traces of TCP, reindents, moves stuff etc.
    
    Remaining buglets:
       - there is not "counter name" column yet
       - the walk for the whole table includes one extra fake value:
    
          snmpwalk -c public -v 2c wrs 1.3.6.1.4.1.96.100.2
          [...]
          SNMPv2-SMI::enterprises.96.100.2.1.18.38 = INTEGER: 60090
          SNMPv2-SMI::enterprises.96.100.2.1.18.39 = INTEGER: 0
          SNMPv2-SMI::enterprises.96.100.2.1.19 = INTEGER: 0
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- userspace/snmpd/pstatsTable.c
+++ userspace/snmpd/pstatsTable.c
@@ -8,8 +8,6 @@
 #include <net-snmp/agent/net-snmp-agent-includes.h>
 #include <net-snmp/agent/auto_nlist.h>
 
-#include <netinet/tcp.h>
-
 #include "pstatsTable.h"
 
 #include <stdarg.h>
@@ -37,8 +35,8 @@ static int logmsg(const char *fmt, ...)
 
 int dumpstruct(FILE *dest, char *name, void *ptr, int size)
 {
-    int ret = 0, i;
-    unsigned char *p = ptr;
+	int ret = 0, i;
+	unsigned char *p = ptr;
 
 	if (WRS_WITH_SNMP_HACKISH_LOG) {
 		ret = fprintf(dest, "dump %s at %p (size 0x%x)\n",
@@ -63,31 +61,140 @@ struct pstats_per_port {
 	uint32_t  val[PSTATS_N_COUNTERS];
 };
 
-struct pstats_global_data {
+static struct pstats_global_data {
 	struct pstats_per_port port[PSTATS_N_PORTS];
 	char *pname[PSTATS_N_PORTS];
 } pstats_global_data;
 
-
-#define	TCPTABLE_ENTRY_TYPE	struct inpcb
-#define	TCPTABLE_STATE		inp_state
-#define	TCPTABLE_LOCALADDRESS	inp_laddr.s_addr
-#define	TCPTABLE_LOCALPORT	inp_lport
-#define	TCPTABLE_REMOTEADDRESS	inp_faddr.s_addr
-#define	TCPTABLE_REMOTEPORT	inp_fport
-
-				/* Head of linked list, or root of table */
-TCPTABLE_ENTRY_TYPE	*tcp_head  = NULL;
-int                      tcp_size  = 0;	/* Only used for table-based systems */
-int                      tcp_estab = 0;
-
-
 #define PSTATS_CACHE_TIMEOUT 5 /* seconds */
+
+
+static int
+pstatsTable_handler(netsnmp_mib_handler          *handler,
+		    netsnmp_handler_registration *reginfo,
+		    netsnmp_agent_request_info   *reqinfo,
+		    netsnmp_request_info         *requests)
+{
+	netsnmp_request_info  *request;
+	netsnmp_variable_list *requestvb;
+	netsnmp_table_request_info *table_info;
+	oid      subid;
+	long     state;
+
+	struct pstats_global_data *data = &pstats_global_data; /* a shorter name */
+	int counter;
+	int wrport;
+	uint32_t *c;
+
+	logmsg("%s: %i\n", __func__, __LINE__);
+	switch (reqinfo->mode) {
+	case MODE_GET:
+		for (request=requests; request; request=request->next) {
+			requestvb = request->requestvb;
+
+			logmsg("%s: %i\n", __func__, __LINE__);
+			/* our "context" is the counter number; "subid" the column i.e. the port */
+			counter = (int)netsnmp_extract_iterator_context(request);
+
+			table_info = netsnmp_extract_table_info(request);
+			wrport = table_info->colnum - 1; /* port is 0-based */
+			logmsg("counter %i, port %i\n", counter, wrport);
+			/* FIXME: the name as first column */
+
+			/* While most tables do "switch(subid)" we'd better just index */
+			c = &data->port[wrport].val[counter];
+			snmp_set_var_typed_value(requestvb, ASN_INTEGER, /* FIXME: counter32 */
+						 (u_char *)c, sizeof(*c));
+		}
+		break;
+
+	case MODE_GETNEXT:
+	case MODE_GETBULK:
+	case MODE_SET_RESERVE1:
+	case MODE_SET_RESERVE2:
+	case MODE_SET_ACTION:
+	case MODE_SET_COMMIT:
+	case MODE_SET_FREE:
+	case MODE_SET_UNDO:
+		/* unsupported mode */
+		break;
+	default:
+		/* unknown mode */
+		break;
+	}
+
+	return SNMP_ERR_NOERROR;
+}
+
+
+static netsnmp_variable_list *
+pstatsTable_next_entry( void **loop_context,
+			void **data_context,
+			netsnmp_variable_list *index,
+			netsnmp_iterator_info *data)
+{
+	long addr, port;
+	intptr_t i;
+
+	/* create the line ID from counter number */
+	i = (intptr_t)*loop_context;
+	//logmsg("%s: %i (i = %i)\n", __func__, __LINE__, i);
+	if (i >= PSTATS_N_COUNTERS)
+		return NULL; /* no more */
+	i++;
+	/* Create the row OID: only the counter index */
+	snmp_set_var_value(index, (u_char*)&i, sizeof(i));
+
+	/* Set the data context (1..39 -> 0..38) */
+	*data_context = (void *)(intptr_t)(i - 1);
+	/* and set the loop context for the next iteration */
+	*loop_context = (void *)i;
+	return index;
+}
+
+static netsnmp_variable_list *
+pstatsTable_first_entry(void **loop_context,
+			void **data_context,
+			netsnmp_variable_list *index,
+			netsnmp_iterator_info *data)
+{
+	logmsg("%s: %i\n", __func__, __LINE__);
+
+	/* reset internal position, so "next" is "first" */
+	*loop_context = (void*)0; /* first counter */
+	return pstatsTable_next_entry(loop_context, data_context, index, data);
+}
+
+static int
+pstatsTable_load(netsnmp_cache *cache, void *vmagic)
+{
+	FILE *f;
+	char fname[32];
+	int wrport, counter;
+	struct pstats_per_port  *stat;
+
+	for (wrport = 0; wrport < PSTATS_N_PORTS; wrport++) {
+		sprintf(fname, "/proc/sys/pstats/port%i", wrport);
+		stat = pstats_global_data.port + wrport;
+		f = fopen(fname, "r");
+		if (!f) {
+			memset(stat, 0x7f, sizeof(*stat));
+			continue;
+		}
+		for (counter = 0; counter < PSTATS_N_COUNTERS; counter++) {
+			if (fscanf(f, "%u", stat->val + counter) != 1)
+				stat->val[counter] = 0xffffff;
+		}
+		fclose(f);
+	}
+	//dumpstruct(logf, "global data", &pstats_global_data, sizeof(pstats_global_data));
+	return 0;
+}
 
 void
 init_pstatsTable(void)
 {
-	const oid pstatsTable_oid[] = {  1, 3, 6, 1, 4, 1, 96, 100, 2, };
+	const oid pstatsTable_oid[] = {  1, 3, 6, 1, 4, 1, 96, 100, 2 };
 
 	netsnmp_table_registration_info *table_info;
 	netsnmp_iterator_info *iinfo;
@@ -101,7 +208,7 @@ init_pstatsTable(void)
 	netsnmp_table_helper_add_indexes(table_info, ASN_INTEGER, 0);
 
 	table_info->min_column = 1;
-	table_info->max_column = PSTATS_N_PORTS;
+	table_info->max_column = PSTATS_N_PORTS; /* FIXME: string missing */
 
 	/* Iterator info */
 	iinfo  = SNMP_MALLOC_TYPEDEF(netsnmp_iterator_info);
@@ -122,119 +229,6 @@ init_pstatsTable(void)
 	/* and create a local cache */
 	netsnmp_inject_handler(reginfo,
 				netsnmp_get_cache_handler(PSTATS_CACHE_TIMEOUT,
-							  pstatsTable_load, pstatsTable_free,
+							  pstatsTable_load, NULL,
 							  pstatsTable_oid, OID_LENGTH(pstatsTable_oid)));
-}
-
-int
-pstatsTable_handler(netsnmp_mib_handler          *handler,
-                 netsnmp_handler_registration *reginfo,
-                 netsnmp_agent_request_info   *reqinfo,
-                 netsnmp_request_info         *requests)
-{
-    netsnmp_request_info  *request;
-    netsnmp_variable_list *requestvb;
-    netsnmp_table_request_info *table_info;
-    TCPTABLE_ENTRY_TYPE	  *entry;
-    oid      subid;
-    long     state;
-
-    struct pstats_global_data *data = &pstats_global_data; /* a shorter name */
-    int counter;
-    int wrport;
-    uint32_t *c;
-
-    switch (reqinfo->mode) {
-    case MODE_GET:
-        for (request=requests; request; request=request->next) {
-            requestvb = request->requestvb;
-
-	    /* our "context" is the counter number; "subid" the column i.e. the port */
-            counter = (int)netsnmp_extract_iterator_context(request);
-   
-            table_info = netsnmp_extract_table_info(request);
-            wrport = table_info->colnum - 1; /* port is 0-based */
-	    logmsg("counter %i, port %i\n", counter, wrport);
-	    /* FIXME: the name as first column */
-
-	    /* While most tables do "switch(subid)" we'd better just index */
-	    data->port[wrport].val[counter]++;
-	    c = &data->port[wrport].val[counter];
-	    snmp_set_var_typed_value(requestvb, ASN_INTEGER, /* FIXME: counter32 */
-				     (u_char *)c, sizeof(*c));
-	}
-        break;
-
-    case MODE_GETNEXT:
-    case MODE_GETBULK:
-    case MODE_SET_RESERVE1:
-    case MODE_SET_RESERVE2:
-    case MODE_SET_ACTION:
-    case MODE_SET_COMMIT:
-    case MODE_SET_FREE:
-    case MODE_SET_UNDO:
-	    /* unsupported mode */
-        break;
-    default:
-	    /* unknown mode */
-	    break;
-	}
-
-    return SNMP_ERR_NOERROR;
-}
-
-netsnmp_variable_list *
-pstatsTable_first_entry(void **loop_context,
-			void **data_context,
-			netsnmp_variable_list *index,
-			netsnmp_iterator_info *data)
-{
-	logmsg("%s: %i\n", __func__, __LINE__);
-
-	/* reset internal position, so "next" is "first" */
-	*loop_context = (void*)0; /* first counter */
-	return pstatsTable_next_entry(loop_context, data_context, index, data);
-}
-
-netsnmp_variable_list *
-pstatsTable_next_entry( void **loop_context,
-			void **data_context,
-			netsnmp_variable_list *index,
-			netsnmp_iterator_info *data)
-{
-	long addr, port;
-	short i;
-
-	//logmsg("%s: %i\n", __func__, __LINE__);
-
-	/* create the line ID from counter number */
-	i = (short)*loop_context;
-	logmsg("%s: %i (i = %i)\n", __func__, __LINE__, i);
-	if (i >= PSTATS_N_COUNTERS)
-		return NULL; /* no more */
-	i++;
-	/* Create the row OID: only the counter index */
-	snmp_set_var_value(index, (u_char*)&i, sizeof(i));
-
-    /* Set the data context (1..39 -> 0..38) */
-    *data_context = (void *)(i - 1);
-    /* and set the loop context for the next iteration */
-    *loop_context = (void *)i;
-    return index;
-}
-
-void
-pstatsTable_free(netsnmp_cache *cache, void *magic)
-{
-	/* nothing to free  */
-}
-
-int
-pstatsTable_load(netsnmp_cache *cache, void *vmagic)
-{
-    FILE           *in;
-    char            line[256];
-
-    /* nothing to load -- FIXME: values */
-    return 0;
 }
--- userspace/snmpd/pstatsTable.h
+++ userspace/snmpd/pstatsTable.h
@@ -1,27 +1,6 @@
 #ifndef _PSTATSTABLE_H
 #define _PSTATSTABLE_H
 
-struct inpcb {
-    struct inpcb   *inp_next;   /* pointers to other pcb's */
-    struct in_addr  inp_faddr;  /* foreign host table entry */
-    u_short         inp_fport;  /* foreign port */
-    struct in_addr  inp_laddr;  /* local host table entry */
-    u_short         inp_lport;  /* local port */
-    int             inp_state;
-    int             uid;        /* owner of the connection */
-};
-
-extern void                init_pstatsTable(void);
-extern Netsnmp_Node_Handler     pstatsTable_handler;
-extern NetsnmpCacheLoad         pstatsTable_load;
-extern NetsnmpCacheFree         pstatsTable_free;
-extern Netsnmp_First_Data_Point pstatsTable_first_entry;
-extern Netsnmp_Next_Data_Point  pstatsTable_next_entry;
-
-#define TCPCONNSTATE	     1
-#define TCPCONNLOCALADDRESS  2
-#define TCPCONNLOCALPORT     3
-#define TCPCONNREMOTEADDRESS 4
-#define TCPCONNREMOTEPORT    5
+extern void init_pstatsTable(void);
 
 #endif /* _PSTATSTABLE_H */
