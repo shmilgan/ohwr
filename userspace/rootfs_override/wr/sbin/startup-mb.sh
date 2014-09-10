b703280c2ab5fd5003d2243cbbbf34bd368a9ef8
Benoit Rat <benoit@sevensols.com>
2014-09-10 14:45:08 +0200

    rt_cpu: load rt_cpu.elf file and change PLL config according to scb_ver
    The script to load the elf file at startup need to be improved but it
    was a quick hack to have things works on SCB v3.3 and SCB v3.4.

--- userspace/rootfs_override/wr/sbin/startup-mb.sh
+++ userspace/rootfs_override/wr/sbin/startup-mb.sh
@@ -12,8 +12,14 @@ done
 # Obtain the type of FPGA (LX130XT or LX240XT)
 tfpga=$($WR_HOME/bin/wrs_version -F)
 
+# TODO: Update wrsw_version to read this value from DF.
+scb_ver=33
+if mtdinfo -a | grep -A 1 dataflash | grep 264 &> /dev/null; then
+	scb_ver=34
+fi
+
 $WR_HOME/bin/load-virtex $WR_HOME/lib/firmware/18p_mb-${tfpga}.bin
-$WR_HOME/bin/load-lm32 $WR_HOME/lib/firmware/rt_cpu.bin
+$WR_HOME/bin/load-lm32 $WR_HOME/lib/firmware/rt_cpu.elf scb_ver=${scb_ver}
 insmod $WR_HOME/lib/modules/at91_softpwm.ko
 insmod $WR_HOME/lib/modules/wr_vic.ko
 insmod $WR_HOME/lib/modules/wr-nic.ko macaddr=$val
