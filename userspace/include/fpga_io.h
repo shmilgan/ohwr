ef3ed69f7c6c05af08e5597a14aab584550b754f
Alessandro Rubini <rubini@gnudd.com>
2014-11-21 10:23:18 +0100

    userspace/include/fpga_io.h: reindent to make it more readable
    This header is almost obsolete, as we'll move to SDB soon.
    Meanwhile, sort out the tab/space mess and indent with spaces only,
    so it looks good to everybody (including greg who insists on one
    tab every 2 spaces -- which works for indentation, but not for
    for alignment at end-of-line).
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- userspace/include/fpga_io.h
+++ userspace/include/fpga_io.h
@@ -3,39 +3,39 @@
 
 #include <stdint.h>
 
-
 /* Base addresses of all FPGA peripherals used in libwr */
 
 /* PPS Generator */
-#define FPGA_BASE_PPS_GEN  0x10500
+#define FPGA_BASE_PPS_GEN      0x10500
 
 /* Routing Table */
-#define FPGA_BASE_RTU 	   0x60000
+#define FPGA_BASE_RTU          0x60000
 
 /* Simple PWM module */
-#define FPGA_BASE_SPWM 		0x55000
+#define FPGA_BASE_SPWM         0x55000
 
 /* HW Info Unit */
-#define FPGA_BASE_HWIU    0x59000
+#define FPGA_BASE_HWIU         0x59000
 
 /* Topology Resolution Unit */
-#define FPGA_BASE_TRU 	  0x56000 // 0x57000
+#define FPGA_BASE_TRU          0x56000
 
 /* Time-Aware Traffic Shaper Unit */
-#define FPGA_BASE_TATSU 	  0x57000 // 0x57000
+#define FPGA_BASE_TATSU        0x57000
 
 /* per-port Statistics */
-#define FPGA_BASE_PSTATS 0x58000
+#define FPGA_BASE_PSTATS       0x58000
 
 /* HardWare Debugging Unit */
-#define FPGA_BASE_HWDU 	  0x59000
+#define FPGA_BASE_HWDU         0x59000
 
 /* Endpoint  */
-#define FPGA_BASE_EP0 	   0x30000
+#define FPGA_BASE_EP0          0x30000
 
-#define FPGA_BASE_ADDR _fpga_base_virt
+extern volatile uint8_t        *_fpga_base_virt; /* libwr/fpga_io.c */
 
-extern volatile uint8_t *_fpga_base_virt;
+#define FPGA_BASE_ADDR          _fpga_base_virt
+
 
 #define _fpga_writel(reg, val){ *(volatile uint32_t *)(FPGA_BASE_ADDR + (reg)) = (val); }
 #define _fpga_readl(reg) (*(volatile uint32_t *)(FPGA_BASE_ADDR + (reg)))
