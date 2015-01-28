136220f0f0667960d3bfe37ec521066fc8ef443e
Adam Wujek <adam.wujek@cern.ch>
2015-01-28 17:27:55 +0100

    userspace/include: remove unused hal's rpc calls
    Removed unused minipc calls:
    -check_running
    -reset_port
    -calibration_cmd
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- userspace/include/hal/hal_exports.h
+++ userspace/include/hal/hal_exports.h
@@ -86,45 +86,11 @@ typedef struct {
 */
 
 /* Prototypes of functions that call on rpc */
-extern int halexp_check_running(void);
-extern int halexp_reset_port(const char *port_name);
-extern int halexp_calibration_cmd(const char *port_name, int command, int on_off);
 extern int halexp_lock_cmd(const char *port_name, int command, int priority);
 extern int halexp_pps_cmd(int cmd, hexp_pps_params_t *params);
 
 /* Export structures, shared by server and client for argument matching */
 #ifdef HAL_EXPORT_STRUCTURES
-
-//int halexp_check_running();
-struct minipc_pd __rpcdef_check_running = {
-	.name = "check_running",
-	.retval = MINIPC_ARG_ENCODE(MINIPC_ATYPE_INT, int),
-	.args = {
-		 MINIPC_ARG_END,
-		 },
-};
-
-//int halexp_reset_port(const char *port_name);
-struct minipc_pd __rpcdef_reset_port = {
-	.name = "reset_port",
-	.retval = MINIPC_ARG_ENCODE(MINIPC_ATYPE_INT, int),
-	.args = {
-		 MINIPC_ARG_ENCODE(MINIPC_ATYPE_STRING, char *),
-		 MINIPC_ARG_END,
-		 },
-};
-
-//int halexp_calibration_cmd(const char *port_name, int command, int on_off);
-struct minipc_pd __rpcdef_calibration_cmd = {
-	.name = "calibration_cmd",
-	.retval = MINIPC_ARG_ENCODE(MINIPC_ATYPE_INT, int),
-	.args = {
-		 MINIPC_ARG_ENCODE(MINIPC_ATYPE_STRING, char *),
-		 MINIPC_ARG_ENCODE(MINIPC_ATYPE_INT, int),
-		 MINIPC_ARG_ENCODE(MINIPC_ATYPE_INT, int),
-		 MINIPC_ARG_END,
-		 },
-};
 
 //int halexp_lock_cmd(const char *port_name, int command, int priority);
 struct minipc_pd __rpcdef_lock_cmd = {
