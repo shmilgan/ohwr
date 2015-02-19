1cd7d786e79b336e6ad5aa51ca6d4674a68286bd
Adam Wujek <adam.wujek@cern.ch>
2015-02-19 19:06:58 +0100

    userspace/include: get rid of PPSI's get_sync_state IPC call
    In PPSI remove structure ptpdexp_sync_state_t, which was used by removed call.
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- userspace/include/ptpd_exports.h
+++ userspace/include/ptpd_exports.h
@@ -7,20 +7,10 @@
 #define PTPDEXP_COMMAND_TRACKING 1
 #define PTPDEXP_COMMAND_MAN_ADJUST_PHASE 2
 
-extern int ptpdexp_get_sync_state(ptpdexp_sync_state_t *state);
 extern int ptpdexp_cmd(int cmd, int value);
 
 /* Export structures, shared by server and client for argument matching */
 #ifdef PTP_EXPORT_STRUCTURES
-
-//int ptpdexp_get_sync_state(ptpdexp_sync_state_t *state);
-struct minipc_pd __rpcdef_get_sync_state = {
-	.name = "get_sync_state",
-	.retval = MINIPC_ARG_ENCODE(MINIPC_ATYPE_STRUCT, ptpdexp_sync_state_t),
-	.args = {
-		MINIPC_ARG_END,
-	},
-};
 
 //int ptpdexp_cmd(int cmd, int value);
 struct minipc_pd __rpcdef_cmd = {
