e0a757fedcf0d1c157a4e6030bb12f38c4c91cc5
Adam Wujek <adam.wujek@cern.ch>
2015-06-23 10:07:50 +0200

    rootfs: split start-daemons.sh.in into init scripts
    Split start-daemons.sh.in into init scripts for:
    --HAL
    --RTUd
    --PPSI
    Since HAL, RTUd, PPSI have already waiting for dependent processess
    implemented, we're allowed to spawn them (semi) simultaneously.
    
    Additionally:
    --Remove start-daemons.sh.in
    --Remove updating start-daemons.sh.in log values in apply_dot-config
    --Create links in /etc/rcS/ to init scripts
    
    NOTE: Be sure that no init script has the same name as running program.
          It may cause problems for "pidof".
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

---
+++ userspace/rootfs_override/etc/init.d/hald.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+dotconfig=/wr/etc/dot-config
+
+start() {
+    echo -n "Starting HAL daemon: "
+
+    if [ -f $dotconfig ]; then
+	. $dotconfig
+    else
+	echo "$0 unable to source dot-config ($dotconfig)!"
+    fi
+
+    WRS_LOG=$CONFIG_WRS_LOG_HAL
+
+    # if empty turn it to /dev/null
+    if [ -z $WRS_LOG ]; then
+	WRS_LOG="/dev/null";
+    fi
+
+    # if a pathname, use it
+    if echo "$WRS_LOG" | grep / > /dev/null; then
+	eval LOGPIPE=\" \> $WRS_LOG 2\>\&1 \";
+    else
+	# not a pathname: use verbatim
+	eval LOGPIPE=\" 2\>\&1 \| logger -t wr-switch -p $WRS_LOG\"
+    fi
+
+# be carefull with pidof, no running script should have the same name as process
+    if pidof wrsw_hal > /dev/null; then
+	# wrsw_hal already running
+	echo "Failed (already running?)"
+    else
+	eval /wr/bin/wrsw_hal $LOGPIPE \&
+	echo "OK"
+    fi
+}
+
+stop() {
+    echo -n "Stopping HAL "
+    start-stop-daemon -K -q --exec /wr/bin/wrsw_hal
+    if [ $? -eq 0 ]; then
+	echo "OK"
+    else
+	echo "Failed"
+    fi
+}
+
+restart() {
+    stop
+    start
+}
+
+case "$1" in
+  start)
+	start
+	;;
+  stop)
+	stop
+	;;
+  restart|reload)
+	restart
+	;;
+  *)
+	echo $"Usage: $0 {start|stop|restart}"
+	exit 1
+	;;
+esac
