ab3c84ceb537f43e90f6bcd1b8e9e65525d763a4
José Luis Gutiérrez <jlgutierrez@ugr.es>
2015-07-01 18:43:27 +0200

    www: adding protocol column to endpointcalibration.php
    Dotconfig now includes protocol description for each endpoint. This has
    been added also to the web interface in endpointcalibration.php

--- userspace/rootfs_override/var/www/endpointcalibration.php
+++ userspace/rootfs_override/var/www/endpointcalibration.php
@@ -33,9 +33,9 @@
 		$formatID = "alternatecolor";
 		$class = "altrowstable firstcol";
 		$infoname = "Endpoint Configuration";
-		$size = "8";
-		
-		$header = array ("WR port","Tx","Rx","Mode","Fiber"); 
+		$size = "7";
+
+		$header = array ("WR port","Protocol","Tx","Rx","Mode","Fiber"); 
 		$matrix = array ("key=CONFIG_PORT00_PARAMS,".$_SESSION["KCONFIG"]["CONFIG_PORT00_PARAMS"], 
 							"key=CONFIG_PORT01_PARAMS,".$_SESSION["KCONFIG"]["CONFIG_PORT01_PARAMS"],
 							"key=CONFIG_PORT02_PARAMS,".$_SESSION["KCONFIG"]["CONFIG_PORT02_PARAMS"],
