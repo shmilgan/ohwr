9f8a2eb232aaeec983733b83b08e2131340c66cd
José Luis Gutiérrez <jlgutierrez@ugr.es>
2015-07-06 20:03:35 +0200

    www: 404 not found for footer.png
    after removing footer.png from the img/ folder (no longer needed), we
    got a 404 error and also a display issue in footer. Now fixed.

--- userspace/rootfs_override/var/www/footer.php
+++ userspace/rootfs_override/var/www/footer.php
@@ -1,4 +1,3 @@
-<img src="img/footnote.gif" class="copyright" alt="White-Rabbit OHR"></a>
 <a href='index.php'><IMG SRC='img/wr_logo.png' align=left ,  vspace=7, hspace=5, width=50 , hight=100 , border=0 , alt='White Rabbit'></a>
 <a href='http://www.ohwr.org/projects/wr-switch-sw' target="_blank"><IMG SRC='img/ohr.png' align=left ,  vspace=7, hspace=5, width=35 , hight=100 , border=0 , alt='OHR'></a>
 <p>White Rabbit Project - Open Hardware and Source Project <a class="footer-link" target="_blank"
