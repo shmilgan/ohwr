a85f66d33d8f97146eff8c3c63d5f610081fdf7d
José Luis Gutiérrez <jlgutierrez@ugr.es>
2015-07-09 13:10:44 +0200

    www: auxclk.php panel name change

--- userspace/rootfs_override/var/www/auxclk.php
+++ userspace/rootfs_override/var/www/auxclk.php
@@ -16,7 +16,7 @@
 </div>
 <div class="rightpanel">
 <div class="rightbody">
-<h1 class="title">WRS Aux Clock<a href='help.php?help_id=auxclk' onClick='showPopup(this.href);return(false);'><img align=right src="./img/question.png"></a></h1>
+<h1 class="title">WRS Aux Clock (Out CLK2)<a href='help.php?help_id=auxclk' onClick='showPopup(this.href);return(false);'><img align=right src="./img/question.png"></a></h1>
 
 	<?php session_is_started() ?>
 		
