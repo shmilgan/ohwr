638d3d3296acfd4091fba02ae07e041339afde4d
Adam Wujek <adam.wujek@cern.ch>
2015-11-09 17:08:01 +0100

    rootfs: support more ways to get dot-config's URL via DHCP
    Support assembling dot-config's URL from DHCP options:
    --the filename
    --the BOOTP next server option
    In this case TFTP protocol is used. It is similar to the pxe booting.
    
    Also update documentation.
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- userspace/rootfs_override/wr/bin/dhcp_get_filename.sh
+++ userspace/rootfs_override/wr/bin/dhcp_get_filename.sh
@@ -11,6 +11,16 @@
 # needed. Then we replace the real file if different.
 tmpdir="/tmp"
 
+# check whether we got path to the dot-config file from DHCP server
 if [ -n "$boot_file" ]; then
+    # check whether the path consists potocol type
+    proto=$(echo "$boot_file" | cut -d: -f 1)
+    if [ "$proto" != "tftp" ] && [ "$proto" != "ftp" ] && [ "$proto" != "http" ] && [ -n "$siaddr" ]; then
+	# There is no protocol type in the path. If $siaddr is not empty, assume
+	# config should come from $siaddr/$boot_file via tftp
+	boot_file=tftp://"$siaddr"/"$boot_file"
+    fi
+    # Path with protocol type (added above or originally included) that was
+    # received from the DHCP server.
     echo $boot_file > "$tmpdir"/dot-config_source_url
 fi
