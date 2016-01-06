b4ce6eb2e06b2bebc7ecfc865ac03d9c55b75775
Adam Wujek <adam.wujek@cern.ch>
2016-01-06 12:01:47 +0100

    rootfs: create script to enable switching
    Switching is enabled when interfaces are up. Bringing up interfaces is delayed
    to be able to configure VLANs first.
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

---
+++ userspace/rootfs_override/etc/init.d/enable_switching.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+start() {
+    echo -n "Enable switching: "
+
+    # bring up all interfaces
+    for i in `ls /sys/class/net | grep wr`
+    do
+	    ifconfig $i up
+    done
+
+    echo "OK"
+}
+
+stop() {
+    echo -n "Disable switching: "
+
+    # bring down all interfaces
+    for i in `ls /sys/class/net | grep wr`
+    do
+	    ifconfig $i down
+    done
+
+    echo "OK"
+}
+
+restart() {
+    stop
+    start
+}
+
+case "$1" in
+  start)
+	start
+	;;
+  stop)
+	stop
+	;;
+  restart|reload)
+	restart
+	;;
+  *)
+	echo $"Usage: $0 {start|stop|restart}"
+	exit 1
+	;;
+esac
