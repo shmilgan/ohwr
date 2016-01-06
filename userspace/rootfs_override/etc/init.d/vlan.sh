4d6d611d8b1e8e14ef5718ff2ea73054fc906657
Adam Wujek <adam.wujek@cern.ch>
2016-01-06 12:03:34 +0100

    rootfs: create dummy script to configure VLANs
    So far there are no VLANs configured in this script, but it is good start.
    When one needs can populate this sctipt on his/her own.
    NOTE: VLANs should be configured before any other traffic is allowed. This
    is done by previous commits.
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

---
+++ userspace/rootfs_override/etc/init.d/vlan.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+start() {
+    echo -n "Setting up VLANs: "
+
+    # set-up VLANs
+    /wr/etc/vlan_config.sh
+
+    if [ $? -eq 0 ]; then
+	echo "OK"
+    else
+	echo "Failed"
+    fi
+}
+
+stop() {
+    echo -n "Clean VLANs' configuration: "
+
+    /wr/bin/wrs_vlans --ep 0-17 --emode 3
+    /wr/bin/wrs_vlans --clear
+
+    echo "OK"
+}
+
+restart() {
+    stop
+    start
+}
+
+case "$1" in
+    start)
+	start
+	;;
+    stop)
+	stop
+	;;
+    restart|reload)
+	restart
+	;;
+    *)
+	echo $"Usage: $0 {start|stop|restart}"
+	exit 1
+	;;
+esac
