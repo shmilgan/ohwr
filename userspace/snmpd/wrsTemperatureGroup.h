3f93266e8b6d58c73a7e65173fd7ce3d7a750258
Adam Wujek <adam.wujek@cern.ch>
2016-07-07 17:15:26 +0200

    userspace/snmpd: (no tech change) rename fields in the wrsTemperature_s
    Rename fields in the struct wrsTemperature_s to reflect SNMP objects'
    names.
    
    temp_fpga -> wrsTempFPGA
    temp_pll -> wrsTempPLL
    temp_psl -> wrsTempPSL
    temp_psr -> wrsTempPSR
    temp_fpga_thold -> wrsTempThresholdFPGA
    temp_pll_thold -> wrsTempThresholdPLL
    temp_psl_thold -> wrsTempThresholdPSL
    temp_psr_thold -> wrsTempThresholdPSR
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- userspace/snmpd/wrsTemperatureGroup.h
+++ userspace/snmpd/wrsTemperatureGroup.h
@@ -5,14 +5,14 @@
 #define WRSTEMPERATURE_OID WRS_OID, 7, 1, 3
 
 struct wrsTemperature_s {
-	int temp_fpga;		/* FPGA temperature */
-	int temp_pll;		/* PLL temperature */
-	int temp_psl;		/* PSL temperature */
-	int temp_psr;		/* PSR temperature */
-	int temp_fpga_thold;	/* Threshold value for FPGA temperature */
-	int temp_pll_thold;	/* Threshold value for PLL temperature */
-	int temp_psl_thold;	/* Threshold value for PSL temperature */
-	int temp_psr_thold;	/* Threshold value for PSR temperature */
+	int wrsTempFPGA;		/* FPGA temperature */
+	int wrsTempPLL;			/* PLL temperature */
+	int wrsTempPSL;			/* PSL temperature */
+	int wrsTempPSR;			/* PSR temperature */
+	int wrsTempThresholdFPGA;	/* Threshold value for FPGA temperature */
+	int wrsTempThresholdPLL;	/* Threshold value for PLL temperature */
+	int wrsTempThresholdPSL;	/* Threshold value for PSL temperature */
+	int wrsTempThresholdPSR;	/* Threshold value for PSR temperature */
 };
 
 extern struct wrsTemperature_s wrsTemperature_s;
