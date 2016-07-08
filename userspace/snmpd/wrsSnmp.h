d433116edd52f23ce5bd6501c9ee39c273e00f90
Adam Wujek <adam.wujek@cern.ch>
2016-07-08 17:15:26 +0200

    userspace/snmpd: add helper macros for verbose messages
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- userspace/snmpd/wrsSnmp.h
+++ userspace/snmpd/wrsSnmp.h
@@ -52,4 +52,59 @@ struct pickinfo {
 	.len = sizeof(((struct _struct *)0)->_field),			\
 	 }
 
+/*
+ * Print a message and execute assignment when the condition is met.
+ *
+ * usage:
+ * _log_type -- on of the SL_* string defines
+ * _st1, _st2 -- structure or pointer to structure to get valid value;
+ *               this field can be empty when _v1/_v2 is a variable
+ * _v1, _v2 -- variable to get the valid value
+ * _comp_sign -- compare operator to be used (like ==, !=, <, >, etc.)
+ * _assign -- assignment to be performed when condition is met
+ * NOTE:
+ * This macro requires slog_obj_name to be defined.
+ *
+ * Valid example1:
+ * strcpy(slog_obj_name, "my_obj");
+ * SLOG_IF_COMP(SL_W, a->,b, ==, c->,d, f = 123);
+ * When condition a->b == c->d is met, macro will assign 123 to f and print
+ * the following message:
+ * SNMP: Warning my_obj: b(11) == d(22)
+ *
+ * Valid example2:
+ * strcpy(slog_obj_name, "my_obj2");
+ * SLOG_IF_COMP(SL_ER, a->,b, <,,d, f = 123);
+ * When condition a->b < d is met, will assign 123 to f and print
+ * the following message:
+ * SNMP: Error my_obj2: b(11) < d(22)
+ */
+#define SLOG_IF_COMP(_log_type, _st1, _v1, _comp_sign, _st2, _v2, _assign) \
+     do { if (_st1 _v1 _comp_sign _st2 _v2) {\
+             _assign; \
+             snmp_log(LOG_ERR, "SNMP: " _log_type " %s: " #_v1 "(%d) " #_comp_sign " " #_v2 "(%d)\n", slog_obj_name, _st1 _v1, _st2 _v2); \
+             } \
+     } while (0)
+
+/*
+ * Print a message about an object
+ *
+ * Valid example2:
+ * strcpy(slog_obj_name, "my_obj");
+ * SLOG(SL_BUG);
+ * prints:
+ * SNMP: BUG my_obj
+ */
+#define SLOG(_log_type) \
+     do { \
+         snmp_log(LOG_ERR, "SNMP: " _log_type " %s\n", slog_obj_name); \
+     } while (0)
+
+/* String definitions for functions SLOG* */
+#define SL_ER "Error"
+#define SL_W "Warning"
+#define SL_NA "Warning NA"
+#define SL_BUG "BUG"
+
+
 #endif /* WRS_SNMP_H */
