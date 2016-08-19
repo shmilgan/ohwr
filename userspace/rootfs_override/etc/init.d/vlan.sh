bca0236aef8d6bb1e65dafcd9516a06b9a93a1d8
Adam Wujek <adam.wujek@cern.ch>
2016-08-19 10:39:59 +0200

    rootfs: read vlan configuration from dot-config in the vlan.sh init script
    Update vlan.sh init script to let wrs_vlans to read configuration from
    the dot-config.
    
    Remove vlan_config.sh since it is not used anymore.
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- userspace/rootfs_override/etc/init.d/vlan.sh
+++ userspace/rootfs_override/etc/init.d/vlan.sh
@@ -1,13 +1,45 @@
 #!/bin/sh
 
+tmpdir=/tmp
+vlans_status_file="$tmpdir"/vlans_status
+
+dotconfig=/wr/etc/dot-config
+
+set -o pipefail
+
 start() {
     echo -n "Setting up VLANs: "
 
-    # set-up VLANs
-    /wr/etc/vlan_config.sh
+    if [ -f $dotconfig ]; then
+	. $dotconfig
+    else
+	echo "$0 unable to start wrs_throttling, unable to source " \
+	      "dot-config ($dotconfig)!"
+	exit 1
+    fi
 
-    if [ $? -eq 0 ]; then
+    WRS_LOG=$CONFIG_WRS_LOG_OTHER
+
+    # if empty turn it to /dev/null
+    if [ -z $WRS_LOG ]; then
+	WRS_LOG="/dev/null";
+    fi
+
+    # if a pathname, use it
+    if echo "$WRS_LOG" | grep / > /dev/null; then
+	eval LOGPIPE=\" \> $WRS_LOG 2\>\&1 \";
+    else
+	# not a pathname: use verbatim
+	eval LOGPIPE=\" 2\>\&1 \| logger -t wr-switch -p $WRS_LOG\"
+    fi
+
+    # set-up VLANs
+    eval /wr/bin/wrs_vlans -f /wr/etc/dot-config $LOGPIPE
+    ret=$?
+    if [ $ret -eq 0 ]; then
 	echo "OK"
+    elif [ $ret -eq 2 ]; then
+	echo "Disabled"
     else
 	echo "Failed"
     fi
