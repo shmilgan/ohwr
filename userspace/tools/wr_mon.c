59bc63d79c8adde7d9db048f60da7ef86c0f9978
Benoit Rat <benoit@sevensols.com>
2017-01-24 12:31:45 +0100

    userspace/tools: wr_mon displays the alpha parameter
    This is usefull to check that the calibration value are correctly
    configured when calibrating the web interface.

--- userspace/tools/wr_mon.c
+++ userspace/tools/wr_mon.c
@@ -561,7 +561,7 @@ void show_servo(int alive)
 			     ppsi_servo_local.delta_ms/1000.0);
 
 		term_cprintf(C_BLUE, "Total link asymmetry: ");
-		term_cprintf(C_WHITE, "%15.3f nsec\n", total_asymmetry/1000.0);
+		term_cprintf(C_WHITE, "%15.3f nsec\t(alpha:%d)\n", total_asymmetry/1000.0,ppsi_servo_local.fiber_fix_alpha);
 
 		/*if (0) {
 			term_cprintf(C_BLUE, "Fiber asymmetry:   ");
