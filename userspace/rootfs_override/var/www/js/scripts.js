736150ac5e9026ff3bcdc840db18feb05c233012
anne <anne@sevensols.com>
2017-02-17 12:37:32 +0100

    www: script.js minor changes
    Added try catch so in case it doesnt find the html tags does not throw and exception

--- userspace/rootfs_override/var/www/js/scripts.js
+++ userspace/rootfs_override/var/www/js/scripts.js
@@ -10,25 +10,30 @@
 function altRows(id){
 	if(document.getElementsByTagName){  
 		var table = document.getElementById(id);  
-		var rows = table.getElementsByTagName("tr"); 
-		 
-		for(i = 0; i < rows.length; i++){          
-			if(i % 2 == 0){
-				rows[i].className += " evenrowcolor";
-			}else{
-				rows[i].className += " oddrowcolor";
-			}      
+		try{
+			var rows = table.getElementsByTagName("tr"); 
+			for(i = 0; i < rows.length; i++){
+                        	if(i % 2 == 0){
+                                	rows[i].className += " evenrowcolor";
+                        	}else{
+                                	rows[i].className += " oddrowcolor";
+                        	}
+                	}
+
+                	var table = document.getElementById(id+1);
+                	var rows = table.getElementsByTagName("tr");
+
+                	for(i = 0; i < rows.length; i++){
+                        	if(i % 2 == 0){
+                                	rows[i].className += " evenrowcolor";
+                        	}else{
+                                	rows[i].className += " oddrowcolor";
+                        	}
+                	}
+
 		}
-		
-		var table = document.getElementById(id+1);  
-		var rows = table.getElementsByTagName("tr"); 
-		 
-		for(i = 0; i < rows.length; i++){          
-			if(i % 2 == 0){
-				rows[i].className += " evenrowcolor";
-			}else{
-				rows[i].className += " oddrowcolor";
-			}      
+		catch(err){
+			console.log(err);
 		}
 	}
 }
