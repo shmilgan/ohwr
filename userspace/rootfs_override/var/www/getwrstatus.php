950cdcee9c52497d5c13113d0a6309bfa24659d8
anne <anne@sevensols.com>
2017-03-01 11:39:08 +0100

    www: wr_mon -w get information improved
    Fixed: Extract WR data in one place ... draw in many!
    Fixed: Replace tail | head by grep if possible

--- userspace/rootfs_override/var/www/getwrstatus.php
+++ userspace/rootfs_override/var/www/getwrstatus.php
@@ -8,27 +8,14 @@
  */
 
 include 'json.php';
+include 'functionsget.php';
 
 $data = array();
 
-#Obtain the wr info
-$wrinfo=shell_exec("/wr/bin/wr_mon -w  | head -19 | tail -18");
-$wrinfo = explode("\n", $wrinfo);
-array_push($data, $wrinfo);
-
-#Obtain the temperatures using the last line of (wr-mon -w)
-$temperatures = shell_exec("/wr/bin/wr_mon -w | tail -1");
-$temperatures = explode(" ",$temperatures);
-array_push($data, $temperatures);
-
-#Obtain wr date
-$wr_date = shell_exec("/wr/bin/wr_date -n get |tail -2");
-$wr_date = explode("\n",$wr_date);
-array_push($data, $wr_date);
-
-$ports = shell_exec("/wr/bin/wr_mon -w | tail -2 | head -1");
-$ports = explode(" ", $ports);
-array_push($data, $ports);
+array_push($data, getPortStatus());
+array_push($data, getTemperatures());
+array_push($data, getWrDate());
+array_push($data, getTiming());
 
 echo __json_encode($data);
 
