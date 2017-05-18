cb82c0d758d37c9dda69b44a73a1bee087a8ddab
Benoit Rat <benoit@sevensols.com>
2017-05-18 18:54:36 +0200

    [BUG: 1570]: www: user can only see sysinfo.php if logged-in

--- userspace/rootfs_override/var/www/footer.php
+++ userspace/rootfs_override/var/www/footer.php
@@ -2,5 +2,9 @@
 <a href='http://www.ohwr.org/projects/wr-switch-sw' target="_blank"><IMG SRC='img/ohr.png' align=left ,  vspace=7, hspace=5, width=35 , hight=100 , border=0 , alt='OHR'></a>
 <p>White Rabbit Project - Open Hardware and Source Project <a class="footer-link" target="_blank"
  href="http://www.ohwr.org/projects/white-rabbit/wiki">White-Rabbit OHR</a>
-<a href='sysinfo.php' target="_blank"><IMG SRC='img/light_php.png' align=right , vspace=7, hspace=5, width=100 , hight=100 , border=0 , alt='PHP Info'></a>
+<?php
+if(isset($_SESSION['myusername'])) echo "<a href='sysinfo.php' target='_blank'>";
+echo "<IMG SRC='img/light_php.png' align=right , vspace=7, hspace=5, width=100 , hight=100 , border=0 , alt='PHP Info'>";
+if(isset($_SESSION['myusername'])) echo "</a>";
+?>
 </p>
