6aebedc3ca115aafd8c2bad03c9caf9c068df09c
Benoit Rat <benoit@sevensols.com>
2017-05-18 18:54:36 +0200

    [BUG: 1570]: www: user can only see sysinfo.php if logged-in

--- userspace/rootfs_override/var/www/sysinfo.php
+++ userspace/rootfs_override/var/www/sysinfo.php
@@ -1,5 +1,31 @@
-<?php
+<?php 
+include 'functions.php';
+ob_start();
+if(strcmp($_SESSION['LASTIME'],filectime($GLOBALS['kconfigfile'])) &&
+		strcmp(basename($_SERVER['PHP_SELF']), "reboot.php")){
+	echo "<script>
+	alert('WARNING: Dotconfig has been modified. Realoading configuration...');
+	window.location.href='index.php';
+	</script>";
+	load_kconfig();
+}
+if (isset($_SESSION['LAST_ACTIVITY'])
+					&& (!empty($_SESSION["myusername"]))
+					&& (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
+    // last request was more than 10 minutes ago
+    session_unset();
+    session_destroy();
+    header('Location: index.php');
+}
+$_SESSION['LAST_ACTIVITY'] = time();
 
-phpinfo();
+if(isset($_SESSION['myusername'])) 
+{
+	phpinfo();
+}
+else
+{
+	header('Location: index.php');
+}
 
 ?>
